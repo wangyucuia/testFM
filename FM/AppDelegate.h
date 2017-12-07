a12c24670dc7511ff054e48d8a804047bb74805c
ayilimi <m15201386258@163.com>
2017-12-07 11:00:41 +0800

    xxx

---
+++ FM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FM
+//
+//  Created by WYC on 2017/12/7.
+//  Copyright © 2017年 WYC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FM
+//
+//  Created by WYC on 2017/12/7.
+//  Copyright © 2017年 WYC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
